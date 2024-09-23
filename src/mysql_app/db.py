ef2d26c8d2b8fbba5f2186f04f46cd91e738bf87
gleyce-alves <gleyce.ufrpe@gmail.com>
2024-09-23 11:04:14 -0300

    fix: add error exception to database connection

--- src/mysql_app/db.py
+++ src/mysql_app/db.py
@@ -2,16 +2,20 @@ import mysql.connector
 import streamlit as st
 
 def connect_db():
-    database = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="programacao_filmes"
-    )
+    try:
+        database = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="140178",
+            database="programacao_filmes"
+        )
 
-    cursor = database.cursor()
-
-    st.session_state.setdefault("cursor", cursor);
-    st.session_state.setdefault("database", database);
-
-    print("Conexão estabelecida")
+        if database.is_connected():
+            cursor = database.cursor()
+            st.session_state["cursor"] = cursor
+            st.session_state["database"] = database
+            print("Conexão estabelecida")
+        else:
+            st.error("Conexão ao banco de dados falhou.")
+    except mysql.connector.Error as err:
+        st.error(f"Erro ao conectar ao banco de dados: {err}")
